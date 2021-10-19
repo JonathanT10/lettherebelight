4b6eed9e57a550b357bf2e8fb866db5ffa9616c7
JonathanT10 <jonathan_terrell@yahoo.com>
2021-10-19 14:20:20 -0500

    Added components folder and begining.jsx.

---
+++ src/components/begining.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Begining extends Component{
+    consturctor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>In the begining</h1>
+            </div>
+        )
+    }
+}
+
+export default Begining;
