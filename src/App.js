5096f9a1ca5017353f3c3d129223fbdbffa0c37f
JonathanT10 <jonathan_terrell@yahoo.com>
2021-10-19 14:38:16 -0500

    beginings.css added some styling to the first page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Router, BrowserRouter } from 'react-router-dom';
+import Begining from './components/begining';
 
 function App() {
   return (
+    <Provider>
     <div>
-     <h1z>Initial set up</h1z>
+    <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Begining} />
+    </Switch>
+    </BrowserRouter>
     </div>
+    </Provider>
   );
 }
 
